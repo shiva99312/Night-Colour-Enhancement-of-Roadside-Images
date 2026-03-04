be5c94c3dcbfa4338ab1ed8a46d14816e039b07d
kittu <shankarshiva99312@gmail.com>
2026-03-04 13:29:33 +0530

    update files

---
+++ app.py
@@ -0,0 +1,132 @@
+import numpy as np
+from PIL import Image
+import image_processing
+import os
+from flask import Flask, render_template, request, make_response
+from datetime import datetime
+from functools import wraps, update_wrapper
+from shutil import copyfile
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+def nocache(view):
+    @wraps(view)
+    def no_cache(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Last-Modified'] = datetime.now()
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
+    return update_wrapper(no_cache, view)
+
+
+@app.route("/index")
+@app.route("/")
+@nocache
+def index():
+    return render_template("home.html", file_path="img/image_here.jpg")
+
+
+@app.route("/about")
+@nocache
+def about():
+    return render_template('about.html')
+
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+
+@app.route("/upload", methods=["POST"])
+@nocache
+def upload():
+    target = os.path.join(APP_ROOT, "static/img")
+    if not os.path.isdir(target):
+        if os.name == 'nt':
+            os.makedirs(target)
+        else:
+            os.mkdir(target)
+    for file in request.files.getlist("file"):
+        file.save("static/img/img_now.jpg")
+    copyfile("static/img/img_now.jpg", "static/img/img_normal.jpg")
+    return render_template("uploaded.html", file_path="img/img_now.jpg")
+
+
+@app.route("/normal", methods=["POST"])
+@nocache
+def normal():
+    copyfile("static/img/img_normal.jpg", "static/img/img_now.jpg")
+    return render_template("uploaded.html", file_path="img/img_now.jpg")
+
+
+@app.route("/grayscale", methods=["POST"])
+@nocache
+def grayscale():
+    image_processing.grayscale()
+    return render_template("uploaded.html", file_path="img/img_now.jpg")
+
+
+
+@app.route("/brightness_addition", methods=["POST"])
+@nocache
+def brightness_addition():
+    image_processing.brightness_addition()
+    return render_template("uploaded.html", file_path="img/img_now.jpg")
+
+
+@app.route("/brightness_substraction", methods=["POST"])
+@nocache
+def brightness_substraction():
+    image_processing.brightness_substraction()
+    return render_template("uploaded.html", file_path="img/img_now.jpg")
+
+
+@app.route("/brightness_multiplication", methods=["POST"])
+@nocache
+def brightness_multiplication():
+    image_processing.brightness_multiplication()
+    return render_template("uploaded.html", file_path="img/img_now.jpg")
+
+
+@app.route("/brightness_division", methods=["POST"])
+@nocache
+def brightness_division():
+    image_processing.brightness_division()
+    return render_template("uploaded.html", file_path="img/img_now.jpg")
+
+
+@app.route("/histogram_equalizer", methods=["POST"])
+@nocache
+def histogram_equalizer():
+    image_processing.histogram_equalizer()
+    return render_template("uploaded.html", file_path="img/img_now.jpg")
+
+
+
+
+@app.route("/histogram_rgb", methods=["POST"])
+@nocache
+def histogram_rgb():
+    image_processing.histogram_rgb()
+    if image_processing.is_grey_scale("static/img/img_now.jpg"):
+        return render_template("histogram.html", file_paths=["img/grey_histogram.jpg"])
+    else:
+        return render_template("histogram.html", file_paths=["img/red_histogram.jpg", "img/green_histogram.jpg", "img/blue_histogram.jpg"])
+
+
+
+if __name__ == '__main__':
+    app.run(port=5001)
